6466ec630a951e044d8d781f1e37a8d0f4cd7b80
Hermann Hahn <hermann.h.hahn@gmail.com>
2022-04-30 16:31:11 -0300

    layout: botão de adicionar

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+
 import "../styles/App.css";
+import Tasks from "./Tasks";
+
+const jsonData = require("../api/database.json");
 
 const App = () => {
+	const [tasks, setTasks] = useState(jsonData);
 	return (
 		<>
-			<div className="container"></div>
+			<h2>Lista de Tarefas</h2>
+			<div className="container">
+				<Tasks tasks={tasks} />
+			</div>
 		</>
 	);
 };
