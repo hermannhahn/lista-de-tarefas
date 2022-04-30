5a00833d3386048c406459e16e63bd5444114061
Hermann Hahn <hermann.h.hahn@gmail.com>
2022-04-30 16:31:49 -0300

    layout: css, botão de adicionar

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import '../styles/AddTask.css';
+import Button from "./Button";
 
 const AddTask = () => {
-	return <></>;
+	return (
+		<>
+			<div className="add-task-container">
+				<input className="add-task-input" type="text" />
+				<div className="add-task-button-container">
+					<Button>+</Button>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default AddTask;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 
-import "../styles/App.css";
 import Tasks from "./Tasks";
+import AddTask from "./AddTask";
 
-const jsonData = require("../api/database.json");
+import "../styles/App.css";
 
 const App = () => {
+	const jsonData = require("../api/database.json");
 	const [tasks, setTasks] = useState(jsonData);
 	return (
 		<>
 			<h2>Lista de Tarefas</h2>
 			<div className="container">
+				<AddTask />
 				<Tasks tasks={tasks} />
 			</div>
 		</>
